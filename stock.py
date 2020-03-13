07bfdc114b9ef779e6339d1a7c6ff02802713be8
Hui Kang <rocksnow1942@gmail.com>
2020-03-13 01:45:50 -0700

    a

--- stock.py
+++ stock.py
@@ -94,7 +94,7 @@ class DataBase():
         list all stocks in [mode] collection
         """
         metas = self[mode].find(projection={'metadata':True})
-        return [i['metadata']['Symbol'] for i in metas]
+        return sorted([i['metadata']['Symbol'] for i in metas])
 
     def changeAllfieldToFloat(self,mode):
         """
@@ -118,18 +118,22 @@ class DataBase():
 
 
 class Stock():
-    def __init__(self,symbol,mode='intraday',shares=1):
+    def __init__(self,symbol,mode='intraday',shares=1,db=None):
         self.symbol = symbol.upper().replace('.','-')
         self.shares = shares
         self.df = None
         self.data = None
-        self.metadata =None
+        self.metadata = None
         self.mode=mode.upper()
+        self.db = db
+        if db:
+            self.readDB()
 
-    def readDB(self,db):
+    def readDB(self,db=None):
         """
         read data of the stock from database.
         """
+        db = db or self.db
         data = db.read(self.symbol,self.mode)
         if data:
             self.metadata = data.pop('metadata')
@@ -139,11 +143,12 @@ class Stock():
         else:
             raise ValueError(f'No data for {self} in database.')
 
-    def savetoDB(self,db,onlyUpdate = True):
+    def savetoDB(self,db=None,onlyUpdate = True):
         """
         save this stock data to db.
         only update data with dates newer than the last update time.
         """
+        db = db or self.db
         lastupdate = db.lastUpdate(self.symbol,self.mode)
         if onlyUpdate:
             toupdate = {k:i for k,i in self.data.items() if (parse(k)>lastupdate)}
@@ -154,12 +159,13 @@ class Stock():
 
     @property
     def lastUpdate(self):
-        return parse(self.metadata['Last Refreshed'])
+        return self.metadata and parse(self.metadata['Last Refreshed'])
 
-    def lastRefresh(self,db):
+    def lastRefresh(self,db=None):
         """
         return time difference in days of current to last refresh time.
         """
+        db = db or self.db
         t = db.lastUpdate(self.symbol,self.mode)
         timedelta = datetime.datetime.now() - t
         return timedelta.days
@@ -197,20 +203,31 @@ class Stock():
             return d
 
     @property
-    def url(self):
+    def url(self,):
+        """
+        determine the API url to call.
+        """
+        size = 'full'
+        now = datetime.datetime.utcnow()
+        if self.mode == 'DAILY':
+            if self.lastUpdate and  ((now - self.lastUpdate).days < 90):
+                size = 'compact'
+        elif self.mode == 'INTRADAY':
+            if self.lastUpdate and (((now - self.lastUpdate).total_seconds() - 3600*4)/300 < 90):
+                size = 'compact'
         if self.mode == 'INTRADAY':
             func = "TIME_SERIES_INTRADAY"
-            return f"https://www.alphavantage.co/query?function={func}&outputsize=full&symbol={self.symbol}&interval=5min&apikey={apikey}"
+            return f"https://www.alphavantage.co/query?function={func}&outputsize={size}&symbol={self.symbol}&interval=5min&apikey={apikey}"
         elif self.mode == 'DAILY':
             func="TIME_SERIES_DAILY_ADJUSTED"
-            return f"https://www.alphavantage.co/query?function={func}&outputsize=full&symbol={self.symbol}&apikey={apikey}"
+            return f"https://www.alphavantage.co/query?function={func}&outputsize={size}&symbol={self.symbol}&apikey={apikey}"
 
     def call_API(self):
         """
         call API from
         """
         url = self.url
-        maxAttempt = 4
+        maxAttempt = 30
         data = None
         while True:
             maxAttempt -=1
@@ -220,7 +237,7 @@ class Stock():
             else:
                 raise ValueError ('No response.')
             if data.get('Meta Data',None): # break if find correct response
-                time.sleep(12) # wait for 12 seconds before next attempt.
+                time.sleep(9) # wait for 10 seconds before next attempt.
                 break
             elif data.get("Error Message",None):
                 print(data)
@@ -229,8 +246,8 @@ class Stock():
                 print(data)
                 return "Maxattempt"
             else:
-                mprint(f'Wait 60seconds. Attempt left {maxAttempt}.')
-                time.sleep(60)
+                mprint(f'Wait 5 seconds. Attempt left {maxAttempt}.')
+                time.sleep(5)
         return data
 
     def parse_data(self,data):
@@ -246,7 +263,7 @@ class Stock():
         self.parse_df()
         return self
 
-    def get_series(self):
+    def sync(self):
         """
         mode can be intraday,daily,weekly or monthly,
         """
@@ -255,7 +272,7 @@ class Stock():
         return self.parse_data(data)
 
 
-    def plot_series(self,toplot=['open','high','low','close'],length=None,period=[],format='auto'):
+    def plot_series(self,toplot=['open','close'],length=None,period=[],format='auto'):
         if format == 'auto':
             if self.mode == 'INTRADAY':
                 format= '%m/%d %H:%M'
@@ -288,17 +305,18 @@ class Stock():
         return self.df.iloc[-1,:].close
 
 class Portfolio():
-    def __init__(self,stocks,mode='intraday'):
+    def __init__(self,stocks,mode='intraday',db=None):
         self.mode = mode.upper()
+        self.db = db
         if isinstance(stocks,list):
             if isinstance(stocks[0],str):
-                self.stocks = [ Stock(i,mode=mode) for i in stocks]
+                self.stocks = [ Stock(i,mode=mode,db=db) for i in stocks]
             else:
                 self.stocks = stocks
         elif isinstance(stocks,dict):
             self.stocks = []
             for k,i in stocks.items():
-                self.stocks.append(Stock(k,shares=i,mode=mode))
+                self.stocks.append(Stock(k,shares=i,mode=mode,db=db))
 
     def __getitem__(self,i):
         if isinstance(i,int):
@@ -320,11 +338,13 @@ class Portfolio():
         for s in self.stocks:
             s.save()
 
-    def savetoDB(self,db):
+    def savetoDB(self,db=None):
+        db = db or self.db
         for s in self.stocks:
             s.savetoDB(db)
 
-    def readDB(self,db):
+    def readDB(self,db=None):
+        db = db or self.db
         for s in self.stocks:
             s.readDB(db)
         return self
@@ -332,15 +352,15 @@ class Portfolio():
     def __repr__(self):
         return "Portfolio:\n"+"\n".join(str(i) for i in self.stocks)
 
-    def get_series(self,):
+    def sync(self,):
         pool = deque(self.stocks)
         while pool:
             s = pool.popleft()
             try:
-                s.get_series()
+                s.sync()
             except Exception as e:
                 print(e,f'Current Deque {pool}')
-                time.sleep(60)
+                time.sleep(10)
             if getattr(s,'df',None) is None:
                 pool.append(s)
         return self
@@ -353,7 +373,7 @@ class Portfolio():
     def currentValue(self):
         return sum(i.currentValue for i in self.stocks)
 
-    def plot_series(self,target='all',toplot=['open','high','low'],length=None,period=[],format='auto'):
+    def plot_series(self,target='all',toplot=['open','close'],length=None,period=[],format='auto'):
         if format == 'auto':
             if self.mode == 'INTRADAY':
                 format= '%m/%d %H:%M'
@@ -394,7 +414,7 @@ def sync_stocks(stocks,db,days=0,mode='intraday',onlyUpdate=True):
         daysSince = stock.lastRefresh(db)
         if daysSince > days:
             print(f"{s} synced {daysSince} days ago.")
-            res = stock.get_series()
+            res = stock.sync()
             if res=='Skip':
                 print(f'Skipped [{s}].')
                 continue
@@ -405,26 +425,27 @@ def sync_stocks(stocks,db,days=0,mode='intraday',onlyUpdate=True):
             else:
                 stock.savetoDB(db,onlyUpdate)
             print(f"Synced stock {s}.")
-    print('Syncing Done.')
 
 def switchapikey():
     global apikey
     apikey = os.environ.get('key2')
 
-def main(d=0):
+def main(d=0,id=0):
     """
-    -d [int]: days threhold to synchronize both daily and intraday data.
+    -d [int]: days threhold to synchronize daily data.
     default: 0
+    -id [int]:days threhold to synchronize intraday data.
     """
     d = int(d)
+    id = int(id)
     db = DataBase()
-    with open('sp500.json','r') as f:
-        symbols = json.load(f)
     print("+"*15+'Syncing DAILY data.'+'+'*15+'\n')
+    symbols = db.listAllStock('DAILY')
     sync_stocks(symbols,db,days=d,mode='Daily')
     print("="*15+'Done DAILY data.'+'='*15+'\n\n\n')
     print("+"*15+'Syncing INTRADAY data.'+'+'*15+'\n')
-    sync_stocks(symbols,db,days=d,mode='intraday')
+    symbols = db.listAllStock('INTRADAY')
+    sync_stocks(symbols,db,days=id,mode='intraday')
     print("="*15+'Done INTRADAY data.'+'='*15+'\n\n')
 
 if __name__=='__main__':
